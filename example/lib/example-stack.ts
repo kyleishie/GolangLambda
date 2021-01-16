299ac5033a0d2732741115e568ae649946121eef
Kyle Ishie <kyleishie@me.com>
2021-01-16 13:39:38 -0500

    Changed to es6

--- example/lib/example-stack.ts
+++ example/lib/example-stack.ts
@@ -1,11 +1,11 @@
 import * as cdk from '@aws-cdk/core';
-import * as golang from "golanglambda";
+import { GolangLambda } from "golanglambda";
 
 export class ExampleStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
-    const myLambda = new golang.GolangLambda(this, 'my-lambda');
+    const myLambda = new GolangLambda(this, 'my-lambda');
   }
 }
